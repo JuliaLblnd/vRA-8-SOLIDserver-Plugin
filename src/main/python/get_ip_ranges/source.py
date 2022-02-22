131748cdb45c2362b52c49fbaa58f67f6f3159d0
Julia Leblond <JuliaLblnd@users.noreply.github.com>
2022-02-22 10:08:03 +0100

    implementing get_ip_ranges

--- src/main/python/get_ip_ranges/source.py
+++ src/main/python/get_ip_ranges/source.py
@@ -12,25 +12,10 @@ conditions of the subcomponent's license, as noted in the LICENSE file.
 import requests
 from vra_ipam_utils.ipam import IPAM
 import logging
+from urllib.parse import parse_qs
+from vra_solidserver_utils.auth import SOLIDserverAuth
+import vra_solidserver_utils as utils
 
-'''
-Example payload:
-
-"inputs": {
-    "endpoint": {
-      "id": "f097759d8736675585c4c5d272cd",
-      "authCredentialsLink": "/core/auth/credentials/13c9cbade08950755898c4b89c4a0",
-      "endpointProperties": {
-        "hostName": "sampleipam.sof-mbu.eng.vmware.com",
-        "certificate": "-----BEGIN CERTIFICATE-----\nMIID0jCCArqgAwIBAgIQQaJF55UCb58f9KgQLD/QgTANBgkqhkiG9w0BAQUFADCB\niTELMAkGA1UEBhMCVVMxEzARBgNVBAgTCkNhbGlmb3JuaWExEjAQBgNVBAcTCVN1\nbm55dmFsZTERMA8GA1UEChMISW5mb2Jsb3gxFDASBgNVBAsTC0VuZ2luZWVyaW5n\nMSgwJgYDVQQDEx9pbmZvYmxveC5zb2YtbWJ1LmVuZy52bXdhcmUuY29tMB4XDTE5\nMDEyOTEzMDExMloXDTIwMDEyOTEzMDExMlowgYkxCzAJBgNVBAYTAlVTMRMwEQYD\nVQQIEwpDYWxpZm9ybmlhMRIwEAYDVQQHEwlTdW5ueXZhbGUxETAPBgNVBAoTCElu\nZm9ibG94MRQwEgYDVQQLEwtFbmdpbmVlcmluZzEoMCYGA1UEAxMfaW5mb2Jsb3gu\nc29mLW1idS5lbmcudm13YXJlLmNvbTCCASIwDQYJKoZIhvcNAQEBBQADggEPADCC\nAQoCggEBAMMLNTqbAri6rt/H8iC4UgRdN0qj+wk0R2blmD9h1BiZJTeQk1r9i2rz\nzUOZHvE8Bld8m8xJ+nysWHaoFFGTX8bOd/p20oJBGbCLqXtoLMMBGAlP7nzWGBXH\nBYUS7kMv/CG+PSX0uuB0pRbhwOFq8Y69m4HRnn2X0WJGuu+v0FmRK/1m/kCacHga\nMBKaIgbwN72rW1t/MK0ijogmLR1ASY4FlMn7OBHIEUzO+dWFBh+gPDjoBECTTH8W\n5AK9TnYdxwAtJRYWmnVqtLoT3bImtSfI4YLUtpr9r13Kv5FkYVbXov1KBrQPbYyp\n72uT2ZgDJT4YUuWyKpMppgw1VcG3MosCAwEAAaM0MDIwMAYDVR0RBCkwJ4cEChda\nCoIfaW5mb2Jsb3guc29mLW1idS5lbmcudm13YXJlLmNvbTANBgkqhkiG9w0BAQUF\nAAOCAQEAXFPIh00VI55Sdfx+czbBb4rJz3c1xgN7pbV46K0nGI8S6ufAQPgLvZJ6\ng2T/mpo0FTuWCz1IE9PC28276vwv+xJZQwQyoUq4lhT6At84NWN+ZdLEe+aBAq+Y\nxUcIWzcKv8WdnlS5DRQxnw6pQCBdisnaFoEIzngQV8oYeIemW4Hcmb//yeykbZKJ\n0GTtK5Pud+kCkYmMHpmhH21q+3aRIcdzOYIoXhdzmIKG0Och97HthqpvRfOeWQ/A\nPDbxqQ2R/3D0gt9jWPCG7c0lB8Ynl24jLBB0RhY6mBrYpFbtXBQSEciUDRJVB2zL\nV8nJiMdhj+Q+ZmtSwhNRvi2qvWAUJQ==\n-----END CERTIFICATE-----\n"
-      }
-    },
-    "pagingAndSorting": {
-      "maxResults": 1000,
-      "pageToken": "789c55905d6e02310c84df7d91452a456481168ec04b55950344f9db55dadd384abc056e5f3b42adfa12299f279ec9ac7c5670e9b0045a4ad2430c93af7a465f3bc83d4f9e3aa8976e6681ce660c827770de2aa1a68c72dfc3cae74393999b2e4df302e72691373aa60199bd827398efac18810f87a952591c61817c849513999df0b6c11436d6d400effcfacc14f2099cd6768913c5a435a0fd0c8e20ab2dbcd147564a2228c93b60b99ae2d94efde6ac640a09d9331130c539367078c41c915067ac9122268dc350439bf3379e9bc01b32025e9bd111aa65c829e89e83f0135ba740572c5f525c73f95faa608e39e55e62c6fcbd37de9775b891212a758d59bceb7a0eb30d7c7f6cd35c1399984291053b30f29fc5feed6cedf7adfe21962ab17b8ebde5089b1fec0d97d7-e5c4e5a1d726f600c22ebfd9f186148a1449755fd79a69ceabfe2aa"
-    }
-  }
-'''
 def handler(context, inputs):
 
     ipam = IPAM(context, inputs)
@@ -39,101 +24,69 @@ def handler(context, inputs):
     return ipam.get_ip_ranges()
 
 def do_get_ip_ranges(self, auth_credentials, cert):
-    # Your implemention goes here
+
     username = auth_credentials["privateKeyId"]
     password = auth_credentials["privateKey"]
+    auth = SOLIDserverAuth(username, password)
 
-    ## If many IP ranges are expected on the IPAM server, it is considered a best practice
-    ## to return them page by page instead of all at once.
-    ## The vRA IPAM Service will propagate a pageToken string with each consecutive request
-    ## until all pages are exhausted
-    pageToken = self.inputs['pagingAndSorting'].get('pageToken', None) ## The first request that vRA sends is with 'None' pageToken
+    properties = utils.get_properties(self.inputs)
 
-    ## Plug your implementation here to collect all the ranges from the external IPAM system
-    result_ranges, next_page_token = collect_ranges(pageToken)
+    service = "/rest/ip_pool_list"
+    url = "https://" + self.inputs["endpoint"]["endpointProperties"]["hostName"] + service
+
+    site_name = properties.get('site_name')
+    params = {}
+    if site_name:
+        params = {"WHERE": "site_name='{}'".format(site_name)}
+
+    response = requests.get(url, params=params, auth=auth, verify=cert)
+
+    result_ranges = []
+
+    for pool in response.json():
+
+        subnet_class_parameters = parse_qs(pool['subnet_class_parameters'])
+
+        # startIPAddress     = utils.long2ip(int(pool["start_ip_addr"], 16))
+        # endIPAddress       = utils.long2ip(int(pool["end_ip_addr"], 16))
+        startIPAddress     = utils.hex2ip(pool["start_ip_addr"])
+        endIPAddress       = utils.hex2ip(pool["end_ip_addr"])
+        subnetPrefixLength = utils.subnet_size2prefix_length(pool["subnet_size"])
+        gatewayAddress     = subnet_class_parameters.get('gateway', [None])[0]
+        dnsSearchDomains   = subnet_class_parameters.get('domain_list', [None])[0].split(";")
+        domain             = subnet_class_parameters.get('domain', [None])[0]
+        dnsServerAddresses = properties.get('dnsServerAddresses', "").replace(" ", "").replace(",", ";").split(";")
+
+        vlan = subnet_class_parameters.get("vlmvlan_vlan_id", ["Not set"])[0]
+
+        description = "VLAN ID: {}".format(vlan)
+
+        logging.debug('Found pool {}'.format(pool["pool_name"]))
+
+        range = {
+            "id"                : pool["pool_id"],      # String, Required
+            "name"              : pool["pool_name"],    # String, Required
+            "description"       : description,          # String, Optional
+            "startIPAddress"    : startIPAddress,       # String, Required
+            "endIPAddress"      : endIPAddress,         # String, Required
+            "ipVersion"         : "IPv4",               # Enum: {IPv4, IPv6}, Required
+            "addressSpaceId"    : "",                   # String, Optional
+            "gatewayAddress"    : gatewayAddress,       # String, Optional
+            "subnetPrefixLength": subnetPrefixLength,   # Integer, Required
+            "dnsServerAddresses": dnsServerAddresses,   # List<String>, Optional
+            "domain"            : domain,               # String, Optional
+            "dnsSearchDomains"  : dnsSearchDomains,     # List<String>, Optional
+            "properties"        : {},                   # Map<String,String>
+            # "tags"              : tags                  # List<Tag>
+        }
+
+        logging.debug(range)
+        result_ranges.append(range)
+
+    logging.info('Found {} IP ranges'.format(len(result_ranges)))
 
     result = {
         "ipRanges": result_ranges
     }
 
-    ## Return the next page token so that vRA can process the first page and then fetch the second page or ranges with the next request
-    if next_page_token is not None:
-        result["nextPageToken"] = next_page_token
-
     return result
-
-def collect_ranges(pageToken):
-    logging.info("Collecting ranges")
-
-    range1 = {
-        "id": "range1",
-
-        "name": "sample name 1",
-
-        "startIPAddress": "10.10.13.1",
-
-        "endIPAddress": "10.10.13.10",
-
-        "description": "sampleDescription",
-
-        "ipVersion": "IPv4",
-
-        "addressSpaceId": "default",
-
-        "subnetPrefixLength": "24",
-
-        "gatewayAddress": "10.10.13.1",
-
-        "domain": "test.local",
-
-        "tags": [{
-            "key": "Building",
-            "value": "VMware main facility"
-        }],
-
-        "properties": {
-        }
-    }
-
-
-    range2 = {
-        "id": "range2",
-
-        "name": "sample name 2",
-
-        "startIPAddress": "10.23.117.1",
-
-        "endIPAddress": "10.23.117.254",
-
-        "description": "sampleDescription",
-
-        "ipVersion": "IPv4",
-
-        "addressSpaceId": "default",
-
-        "subnetPrefixLength": "24",
-
-        "dnsServerAddresses": ["10.10.17.3", "10.10.22.1"],
-
-        "dnsSearchDomains": ["com", "net", "test.local"],
-
-        "domain": "test.local",
-
-        "tags": [{
-            "key": "Building",
-            "value": "VMware main facility"
-        }],
-
-        "properties": {
-        }
-    }
-
-    result = []
-    next_page_token = None
-    if pageToken is None:
-        result = [range1]
-        next_page_token = "87811419dec2112cda2aa29685685d650ac1f61f"
-    else:
-        result = [range2]
-
-    return result, next_page_token
